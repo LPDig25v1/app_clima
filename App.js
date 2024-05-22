ea0a1e07e69b9797787d375c1d45a2c65372ea04
Bruno Ferreira da Silva <bruno_silva2711@yahoo.com.br>
2024-05-21 21:43:27 -0300

    Criação app clima

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, TextInput, Button, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+  const endpoint = "https://api.openweathermap.org/data/2.5/forecast?lang=pt_br&units=metric&q="
+  const apiKey = "fbbd5d253f1a842fad0717949e91d608"
+  const [cidade, setCidade] = useState('')
+  const capturarCidade = (cidade) => {
+    setCidade(cidade)
+  }
+  const [previsoes, setPrevisoes] = useState([]);
+  const obtemPrevisoes = () => {
+    setPrevisoes([]);
+    const target = endpoint + cidade + "&appid=" + apiKey;
+    fetch(target)
+    .then((dados) => dados.json())
+    .then((dados) => {
+      setPrevisoes(dados["list"])
+      Keyboard.dismiss()
+    });
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.entrada}>
+        <TextInput
+          style={styles.nomeCidade}
+          placeholder='Digite o nome de uma cidade'
+          value={cidade}
+          onChangeText={capturarCidade}
+          />
+          <Button 
+            title="Ok"
+            onPress={obtemPrevisoes}
+          />
+      </View>
+      <FlatList
+        data={previsoes}
+        renderItem={
+          previsao => (
+            <Text>{JSON.stringify(previsao)}</Text>
+          )
+        }
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 40,
+    flexDirection: 'column',
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  nomeCidade: {
+    padding: 10,
+    borderBottomColor: '#BB96F3',
+    borderBottomWidth: '2',
+    textAlign: 'left',
+    flexGrow: 0.9,
+  },
+  entrada: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 8,
   },
 });
